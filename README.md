a79d9fb650b3573647431fc8b835fe5c1860440f
yves <yv.desousa@gmail.com>
2020-11-02 19:38:17 -0500

    v2.cyberpunk-vampire

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This is a start page, so I can access my most accessed links, and leave my Bookm
 
 # Changes
 
+## v.2.0 "cyberpunk vampire" 2020-02-11
+
+sp00py
+
 ## v.1.0 "LG-6" 2020-15-05
 
 A lovely initial version, styled off of Lady Gaga's upcoming sixth album, _Chromatica_. Remember to stream [Stupid Love](https://www.youtube.com/watch?v=5L6xyaeiV58) for good skin, and successful builds! 💕
--- js/startPage.js
+++ js/startPage.js
@@ -1,15 +1,15 @@
-const hourDisplay = document.getElementById("hours");
-const periodDisplay = document.getElementById("period");
+const hourDisplay = document.getElementById('hours')
+const periodDisplay = document.getElementById('period')
 
-function setTime(){
-    const time = new Date();
-    const fullTime = time.toLocaleTimeString('en-US').split(" ");
-    const hours = `${fullTime[0]}-`;
-    const period = fullTime[1];
-    hourDisplay.innerHTML = hours;
-    periodDisplay.innerHTML = period;
+function setTime() {
+  const time = new Date()
+  const fullTime = time.toLocaleTimeString('en-US').split(' ')
+  const hours = `${fullTime[0]}`
+  const period = fullTime[1]
+  hourDisplay.innerHTML = hours
+  periodDisplay.innerHTML = period
 }
 
-setInterval(setTime, 1000);
+setInterval(setTime, 1000)
 
-setTime();
+setTime()
